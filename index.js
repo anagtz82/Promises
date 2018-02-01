e71f1e3f4174b645139730e5c2172a708febf150
anagtz82 <anagtz82@me.com>
2018-02-01 16:09:41 -0600

    finished

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
-slowMath.add(1, 2)
+slowMath.add(6, 2)
     .then(
         (sum) => {
-            return slowMath.subtract(sum, 1);
-        },
-        (err) => {
-            console.log(err);
+            console.log(`The sum is ${sum}`);
+            return slowMath.multiply(sum, 2);
+            })
+    .then(
+        (times) =>{
+            console.log(`The multiplied result is ${times}`);
+            return slowMath.divide(times, 4);
+            })
+    .then(
+        (div) => {
+            console.log(`Is divisible by ${div}`);
+            return slowMath.subtract(div, 3);
+            })
+    .then(
+        (sub) => {
+            console.log(`Minus ${sub}`);
+            return slowMath.add(sub, 98);
+            })
+    .then(
+        (divide) => {
+            console.log(`The dividing result is ${divide}`);
+            return slowMath.divide(divide, 99);
+        })
+    .then(
+        (mult) => {
+            console.log(`The multiplication is ${mult}`);
+            return slowMath.multiply(mult, 50);
+        })
+    .then(
+        (div) => {
+            console.log(`The division is ${div}`);
+            return slowMath.remainder(div, 40);
+        })
+    .then(
+        (sum) => {
+            console.log(`The sum is ${sum}`);
+            return slowMath.add(sum, 32);
         })
     .then(
         (difference) => {
-            console.log(`the result of 3-1 is ${difference}`);
+            console.log(`The final result is ${difference}, where you fill in the blank with the final result.`);
         })
     .catch((err) => {
         console.log('err; ' + err);
-    })
+        })
+
+
+
+
+
+     
+
+
+    
+    // System.login(“user”, “pass”)
+    // .then(
+    //     function(success) {
+    //         return someData.save();
+    //         }).then(function(success) {
+    //         return someData.delete();
+    //         }).then(function(success) {
+    //          console.log(‘deleted!’);
+    //         }).catch(function(error) {
+    //         console.log(error);
+    //     });
